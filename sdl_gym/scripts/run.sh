5e04bbd063559b6e904415ff37b243a508dbcd40
YeungYathin <yixuan.yang@duke.edu>
2025-02-04 14:42:36 -0500

    Contact force RL policy env create!

--- sdl_gym/scripts/run.sh
+++ sdl_gym/scripts/run.sh
@@ -1,19 +1,34 @@
 # Activate the Python environment if needed
 
 # Knob Task
-# Train
+
+############### These 5 are IK implementation ###############
 # python train.py --task=g1_knob_1 --num_envs=64
 # python train.py --task=g1_knob_2 --num_envs=64
 # python train.py --task=g1_knob_3 --num_envs=64
 # python train.py --task=g1_knob_4 --num_envs=64
 # python train.py --task=g1_knob_5 --num_envs=64
+#############################################################
 
-# python train.py --task=g1_knob_6 --num_envs=4096 --headless
 
 # Policy-Basic-0
-python play.py --task=g1_knob_6 --num_envs=64 --load_run='Feb04_13-11-34_'
+# Train
+# python train.py --task=g1_knob_6 --num_envs=4096
+# Play
+# python play.py --task=g1_knob_6 --num_envs=64 --load_run='Feb04_13-11-34_'
+
 
 # Policy-1
+# Train
+python train.py --task=g1_knob_7 --num_envs=4096
+# Play
+# python play.py --task=g1_knob_7 --num_envs=64 --load_run='Feb04_13-11-34_'
 
 
+
+
+
+
+
+# DEBUG
 # python -m debugpy --listen 0.0.0.0:5678 --wait-for-client play.py --task=g1_knob_6 --num_envs=2 --headless
